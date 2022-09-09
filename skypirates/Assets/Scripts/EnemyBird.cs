9ef5555f4377717e490ff91bb7379974f2915730
aseay4 <86761894+aseay4@users.noreply.github.com>
2022-09-09 13:32:34 -0400

    Bird Enemy

---
+++ skypirates/Assets/Scripts/EnemyBird.cs
@@ -0,0 +1,45 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyBird : Enemy
+{
+    Rigidbody2D rigid;
+    float directionTimer;
+    float newTime = 5;
+    int direction = 1;
+    float speed = 3;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        rigid = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        directionTimer -= Time.deltaTime;
+
+        if (directionTimer < 0)
+        {
+            direction = -direction;
+            directionTimer = newTime;
+        }
+
+        Movement();
+    }
+
+    private void Movement()
+    {
+        Vector2 position = rigid.position;
+
+        position.x = position.x + Time.deltaTime * speed * direction;
+
+        transform.localScale = new Vector3(-direction, 1, 1); // flip the gameobject to face correctly
+
+        rigid.MovePosition(position);
+    }
+
+
+}
