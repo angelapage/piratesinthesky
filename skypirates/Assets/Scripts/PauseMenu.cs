46edfa8a2092b66a7082b69953c7bba9e05092c7
EmilieGanmont <77340577+EmilieGanmont@users.noreply.github.com>
2022-09-12 21:34:31 -0400

    Pause Menu in Level One

---
+++ skypirates/Assets/Scripts/PauseMenu.cs
@@ -0,0 +1,31 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PauseMenu : MonoBehaviour
+{
+    void Update()
+    {
+        if((Input.GetKeyDown(KeyCode.E)) && !isPaused)
+        {
+            pauseMenu.SetActive(true);
+            Time.timeScale = 0;
+            isPaused = true;
+
+            AudioListener.pause = true;
+        }    
+        else if((Input.GetKeyDown(KeyCode.E)) && isPaused)
+        {
+            pauseMenu.SetActive(false);
+            Time.timeScale = 1;
+            isPaused = false;
+
+            AudioListener.pause = false;
+        }
+    }
+
+    [SerializeField]
+    GameObject pauseMenu;
+
+    private bool isPaused = false;
+}
