6146fb2044986da4632ed4a28775d1323b707b30
angelapage <angelapage1617@gmail.com>
2022-09-21 17:20:22 -0400

    added restart to win scene

---
+++ skypirates/Assets/Scripts/Restart.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Restart : MonoBehaviour
+{
+    AudioSource audioSource;
+    public AudioClip buttonclick;
+
+    void Start()
+    {
+        audioSource = GetComponent<AudioSource>();
+    }
+    public void RestartGame()
+    {
+        audioSource.PlayOneShot(buttonclick);
+        SceneManager.LoadScene(1);
+    }
+
+     public void PlaySound(AudioClip clip)
+    {
+        audioSource.PlayOneShot(clip);
+    }
+}
