b1a3aa94e48572a54fc871413985687d76315025
EmilieGanmont <77340577+EmilieGanmont@users.noreply.github.com>
2022-09-20 20:09:01 -0400

    Fixed bugged screen transitions and added buoy bumper art

--- skypirates/Assets/Scripts/LevelLoader.cs
+++ skypirates/Assets/Scripts/LevelLoader.cs
@@ -2,12 +2,17 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 public class LevelLoader : MonoBehaviour
 {   
     public Animator transition;
     public float transitionTime;
     
+    [SerializeField]
+    private GameObject screenSwiper;
+
+    public Sprite newArt;
 
 
 
@@ -17,12 +22,22 @@ public class LevelLoader : MonoBehaviour
     }
 
     IEnumerator LoadLevel(int levelIndex)
-    {
+    {   
+        if(screenSwiper != null && newArt != null)
+        {
+            ChangeArt();
+        }
+        
         transition.SetTrigger("Start");
 
         yield return new WaitForSeconds(transitionTime);
 
         SceneManager.LoadScene(levelIndex);
+    }
+
+    private void ChangeArt()
+    {
+        screenSwiper.GetComponent<Image>().sprite = newArt;
     }
 
 
